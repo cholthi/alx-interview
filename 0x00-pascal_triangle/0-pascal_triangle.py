4fe7b92f925962a5d6748fbeede2e470e3496bd9
cholthi <cholthipaul@gmail.com>
2024-03-07 22:46:56 +0200

    alx mock interview

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,9 +7,9 @@ def pascal_triangle(n):
         return matrix
     matrix = [[1]]
 
-        for i in range(1, n):
+    for i in range(1, n):
         temp = [1]
-                for j in range(len(matrix[i - 1]) - 1):
+        for j in range(len(matrix[i - 1]) - 1):
             curr = matrix[i - 1]
             temp.append(matrix[i - 1][j] + matrix[i - 1][j + 1])
         temp.append(1)
