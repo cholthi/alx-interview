07634200506c91dc6b0534e94339a3c54bc1d7f2
cholthi <cholthipaul@gmail.com>
2024-04-06 16:47:07 +0200

    fixed pycodestyle errors in 0-validate_utf8.py file

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """ Provides a function to validate if data is in
 valid utf-8 encoding"""
+from typing import List
 
 
-def validUTF8(data):
+def validUTF8(data: List) -> bool:
     """validates if data is valid utf-8 encoding"""
     n_bytes = 0
-    for b in data:
+    for byte in data:
         if n_bytes > 0:
-            if b >> 6 != 0b10:
+            if byte >> 6 != 0b10:
                 return False
             n_bytes -= 1
         else:
-            if b >> 7 == 0:
+            if byte >> 7 == 0:
                 n_bytes = 0
-            elif b >> 5 == 0b110:
+            elif byte >> 5 == 0b110:
                 n_bytes = 1
-            elif b >> 4 == 0b1110:
+            elif byte >> 4 == 0b1110:
                 n_bytes = 2
-            elif b >> 3 == 0b11110:
+            elif byte >> 3 == 0b11110:
                 n_bytes = 3
             else:
                 return False
