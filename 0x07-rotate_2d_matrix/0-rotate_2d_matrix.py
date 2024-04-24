c8008c10acf277b4899d74b116f4fd6be3f305e8
cholthi <cholthipaul@gmail.com>
2024-04-24 14:38:12 +0200

    added the solution

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Provides function to rotate 2d matrix
+"""
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+    """ Roate 2d matrix
+    """
+    no_rows = len(matrix)
+    for i in range(no_rows):
+        for j in range(i, no_rows):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(no_rows):
+        matrix[i] = matrix[i][::-1]
