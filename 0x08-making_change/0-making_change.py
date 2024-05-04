4f6be3055def82e7f6c632772f3521c9ca0b4458
cholthi <cholthipaul@gmail.com>
2024-05-04 14:08:43 +0300

    coin change correction

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -15,14 +15,18 @@ def makeChange(coins: List[int], total: int) -> int:
 
     Return: int - Num of coins for change
     """
-    def coinChangeInner(rem, cache):
-        if rem < 0:
-            return math.inf
-        if rem == 0:
-            return 0
-        if rem in cache:
-            return cache[rem]
-        cache[rem] = min(coinChangeInner(rem-x, cache) + 1 for x in coins)
-        return cache[rem]
-    ans = coinChangeInner(total, {})
-    return -1 if ans == math.inf else ans
+    def change(n):
+        if F[n] != -1:
+            return F[n]
+        value = float('inf')
+        for coin in coins:
+            if coin <= n:
+                value = min(change(n-coin), value)
+        F[n] = value + 1 if value != -1 else -1
+        return F[n]
+    if total == 0:
+        return 0
+    F = [-1 for _ in range(total + 1)]
+    F[0] = 0
+    change(total)
+    return F[-1] if F[-1] != float('inf') else -1
